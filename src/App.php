856290a080795d1d565910e342c29a8f075abf3c
v-dem <vitaly_demyanenko@yahoo.com>
2017-05-22 09:52:50 +0300

    route resolving code moved to a special class

--- src/App.php
+++ src/App.php
@@ -7,15 +7,6 @@ use queasy\log\Logger;
 class App
 {
 
-    const BASE_CONTROLLER_PATH = 'app' . DIRECTORY_SEPARATOR . 'controllers';
-    const BASE_CONTROLLER_NAMESPACE = 'app\\controllers';
-
-    const DEFAULT_CONTROLLER_NAME = 'Default';
-
-    const CONTROLLER_NAMESPACE_TEMPLATE = '%s\\%s';
-    const CONTROLLER_NAME_TEMPLATE = self::CONTROLLER_NAMESPACE_TEMPLATE . 'Controller';
-    const CONTROLLER_PATH_TEMPLATE = '%s' . DIRECTORY_SEPARATOR . '%sController.php';
-
     private $route;
     private $method;
 
@@ -36,7 +27,8 @@ class App
         try {
             $this->before();
 
-            $controllerClass = $this->resolveRoute($this->route);
+            $route = new Route($this->route, $this->method);
+            $controllerClass = $route->resolve();
             if (false === $controllerClass) {
                 throw new Exception('Can\t resolve route.'); // TODO: Change to 404 response
             }
@@ -45,9 +37,9 @@ class App
             $method = strtolower($this->method);
 
             if (method_exists($controller, $method)) {
-                $output = call_user_func_array(array($controller, $method), $this->route);
+                $output = call_user_func_array(array($controller, $method), $route->route());
             } else {
-                $output = call_user_func_array($controller, $this->route);
+                $output = call_user_func_array($controller, $route->route());
                 // throw new ApplicationException(sprintf('Method "%s" doesn\'t exists in class "%s".', $method, $controllerClass));
             }
 
@@ -69,38 +61,6 @@ class App
 
     protected function after()
     {
-    }
-
-    private function resolveRoute(array $route, $path = self::BASE_CONTROLLER_PATH, $namespace = self::BASE_CONTROLLER_NAMESPACE)
-    {
-        $token = array_shift($route);
-        if (!empty($token)) {
-            // Look for controller in subfolder
-            $newPath = $path . DIRECTORY_SEPARATOR . $token;
-            if (@is_dir($newPath)) {
-                $result = $this->resolveRoute($route, $newPath, sprintf(self::CONTROLLER_NAMESPACE_TEMPLATE, $namespace, $token));
-                if (false !== $result) {
-                    return $result;
-                }
-            }
-
-            // Check if controller exists in current folder
-            if (preg_match('/[_a-z]/i', $token)) {
-                if (@file_exists(sprintf(self::CONTROLLER_PATH_TEMPLATE, $path, ucfirst($token)))) {
-                    $this->route = $route;
-
-                    return sprintf(self::CONTROLLER_NAME_TEMPLATE, $namespace, ucfirst($token));
-                }
-            }
-        }
-
-        if (@file_exists(sprintf(self::CONTROLLER_PATH_TEMPLATE, $path, self::DEFAULT_CONTROLLER_NAME))) {
-            $this->route = $route;
-
-            return sprintf(self::CONTROLLER_NAME_TEMPLATE, $namespace, self::DEFAULT_CONTROLLER_NAME);
-        }
-
-        return false;
     }
 
 }
---
+++ src/Route.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace queasy;
+
+use queasy\log\Logger;
+
+class Route
+{
+
+    const BASE_CONTROLLER_PATH = 'app' . DIRECTORY_SEPARATOR . 'controllers';
+    const BASE_CONTROLLER_NAMESPACE = 'app\\controllers';
+
+    const DEFAULT_CONTROLLER_NAME = 'Default';
+
+    const CONTROLLER_NAMESPACE_TEMPLATE = '%s\\%s';
+    const CONTROLLER_NAME_TEMPLATE = self::CONTROLLER_NAMESPACE_TEMPLATE . 'Controller';
+    const CONTROLLER_PATH_TEMPLATE = '%s' . DIRECTORY_SEPARATOR . '%sController.php';
+
+    private $route;
+    private $method;
+
+    public function __construct(array $route, $method)
+    {
+        $this->route = $route;
+        $this->method = $method;
+    }
+
+    public function resolve(array $route = null, $path = self::BASE_CONTROLLER_PATH, $namespace = self::BASE_CONTROLLER_NAMESPACE)
+    {
+        if (is_null($route)) {
+            $route = $this->route;
+        }
+
+        $token = array_shift($route);
+        if (!empty($token)) {
+            // Look for controller in subfolder
+            $newPath = $path . DIRECTORY_SEPARATOR . $token;
+            if (@is_dir($newPath)) {
+                $result = $this->resolve($route, $newPath, sprintf(self::CONTROLLER_NAMESPACE_TEMPLATE, $namespace, $token));
+                if (false !== $result) {
+                    return $result;
+                }
+            }
+
+            // Check if controller exists in current folder
+            if (preg_match('/[_a-z]/i', $token)) {
+                if (@file_exists(sprintf(self::CONTROLLER_PATH_TEMPLATE, $path, ucfirst($token)))) {
+                    $this->route = $route;
+
+                    return sprintf(self::CONTROLLER_NAME_TEMPLATE, $namespace, ucfirst($token));
+                }
+            }
+        }
+
+        if (@file_exists(sprintf(self::CONTROLLER_PATH_TEMPLATE, $path, self::DEFAULT_CONTROLLER_NAME))) {
+            $this->route = $route;
+
+            return sprintf(self::CONTROLLER_NAME_TEMPLATE, $namespace, self::DEFAULT_CONTROLLER_NAME);
+        }
+
+        return false;
+    }
+
+}
+
