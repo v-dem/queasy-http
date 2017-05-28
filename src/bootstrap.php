5266a67a2144c3cd648c829df51980eeb362aa59
v-dem <vitaly_demyanenko@yahoo.com>
2017-05-28 18:25:06 +0300

    #14 done, #12 in progress

---
+++ src/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once(VENDOR_DIR . DIRECTORY_SEPARATOR . 'autoload.php');
+
+chdir(QUEASY_DIR);
+
+$routeStr = preg_replace(
+    '/\?.*$/',
+    '',
+    str_replace(
+        $queasyUrl = str_replace(
+            basename(__FILE__),
+            '',
+            $_SERVER['SCRIPT_NAME']
+        ),
+        '',
+        $_SERVER['REQUEST_URI']
+    )
+);
+
+define('QUEASY_URL', $queasyUrl);
+
+$route = explode('/', $routeStr);
+
+session_start();
+
+queasy\log\Logger::info(sprintf('Request: %s %s', $_SERVER['REQUEST_METHOD'], empty($routeStr)? '/': $routeStr));
+
+$request = new queasy\HttpRequest($_GET, $_POST, $_FILES, $_SESSION);
+
+$appClass = queasy\Loader::load('app');
+$app = new $appClass($route, $_SERVER['REQUEST_METHOD']);
+$app->handle($request);
+
+queasy\log\Logger::info(sprintf('Script execution time: %s', microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']));
+
