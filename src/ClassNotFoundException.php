d245e31543f50db4903d2fcce7db4d1a728efc54
v-dem <vitaly_demyanenko@yahoo.com>
2017-09-04 15:57:18 +0300

    added files

--- src/ClassNotFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace queasy;
-
-class ClassNotFoundException extends SystemException
-{
-
-}
-
--- src/SystemException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace queasy;
-
-class SystemException extends \Exception
-{
-
-}
-
