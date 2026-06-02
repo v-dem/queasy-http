d84e4f2082b4bb85871b570efcc31ed37457796e
Vitaly Demyanenko <vitaly_demyanenko@yahoo.com>
2026-06-02 10:53:45 +0300

    preparing for release

---
+++ src/RequestFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace queasy\http;
+
+class RequestFactory
+{
+    /**
+     * Create a new request.
+     *
+     * @param string $method The HTTP method associated with the request.
+     * @param UriInterface|string $uri The URI associated with the request. If
+     *     the value is a string, the factory MUST create a UriInterface
+     *     instance based on it.
+     *
+     * @return RequestInterface
+     */
+    public function createRequest(string $method, $uri): RequestInterface
+    {
+        $request = new Request();
+
+        // return new 
+    }
+}
